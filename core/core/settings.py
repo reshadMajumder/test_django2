3a887ca509216437d50cfd29e959b6d02ab60c43
Jahidul Hassan Reshad <127494557+reshadMajumder@users.noreply.github.com>
2025-06-02 01:27:21 +0600

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1&#8jehu0%zd^84&vzjbit4p6rd4+8gu0%b-n8aj5s7lr+9lim'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'  # Convert string to boolean
 
 ALLOWED_HOSTS = ["*"]
 
