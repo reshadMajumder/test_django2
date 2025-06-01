f55b96e155bbae945db271c732acf3050bd97282
Jahidul Hassan Reshad <127494557+reshadMajumder@users.noreply.github.com>
2025-06-02 00:00:47 +0600

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1&#8jehu0%zd^84&vzjbit4p6rd4+8gu0%b-n8aj5s7lr+9lim
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -115,8 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
